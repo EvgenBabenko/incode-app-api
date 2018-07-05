17999f8385e20ea91defd463a4b7f3782e8e4aaa
EvgenBabenko <evgen.babenko@gmail.com>
2018-07-05 16:57:57 +0300

    base auth

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,6 +1,6 @@
 const accessControlAllow = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 };
